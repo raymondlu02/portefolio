e9ad413c85563ba52e51099563040bc4a919af77
Raymond <raymond.lu@ouidou.fr>
2024-10-07 12:00:35 +0200

    add on click in css3DRenderer

--- angularPortefolio/src/app/app.component.ts
+++ angularPortefolio/src/app/app.component.ts
@@ -24,120 +24,12 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     if (typeof window!=="undefined") {
-      // browser code
-      // -----------------------------------------------------------------------------------------
-      // const width = window.innerWidth, height = window.innerHeight;
-      //
-      // // init
-      //
-      //
-      // const camera = new THREE.PerspectiveCamera(70, width / height, 1, 150);
-      // camera.position.z = 2;
-      //
-      // const scene = new THREE.Scene();
-      //
-      // const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      // const material = new THREE.MeshNormalMaterial();
-      //
-      // const mesh = new THREE.Mesh(geometry, material);
-      // // scene.add( mesh );
-      //
-      // const renderer = new THREE.WebGLRenderer({antialias: true});
-      //
-      // const cssrenderer = new CSS3DRenderer();
-      //
-      // const controls = new OrbitControls( camera, renderer.domElement);
-      //
-      // renderer.setSize(width, height);
-      // renderer.setClearColor( 0x000000, 0 ); // the default
-      // const animate = (time: number) => {
-      //
-      //   mesh.rotation.x = time / 2000;
-      //   mesh.rotation.y = time / 1000;
-      //
-      //   // camera.lookAt(0, 0, 0);
-      //   renderer.render(scene, camera);
-      //   cssrenderer.render(scene, camera);
-      //   requestAnimationFrame(animate);
-      //
-      // }
-      //
-      // renderer.setAnimationLoop(animate);
-      //
-      //
-      // // charge le modele 3D
-      // // Texture -> faudra que je cherceh ca serrt  a quoi
-      // renderer.toneMapping = THREE.ACESFilmicToneMapping;
-      // renderer.toneMappingExposure = 4;
-      //
-      // const loader = new GLTFLoader();
-      // const rgbeLoader = new RGBELoader();
-      // rgbeLoader.load('./assets/MR_INT-005_WhiteNeons_NAD.hdr', function(texture){
-      //
-      //   texture.mapping = THREE.EquirectangularReflectionMapping;
-      //   scene.environment = texture;
-      //
-      //   // importer un objet 3D
-      //   loader.load('./assets/screen/scene.gltf', function (gltf) {
-      //     // scene.add(gltf.scene); // Ajouter le modèle à la scène
-      //   }, undefined, function (error) {
-      //     console.error(error); // Gérer les erreurs
-      //   });
-      // })
-      //
-      // // -------------------------------------------------------------------
-      //
-      // // Ajout des axes (axes helper)
-      // const axesHelper = new THREE.AxesHelper(5); // Longueur des axes: 5 unités
-      // scene.add(axesHelper);
-      //
-      //
-      // document.body.appendChild(renderer.domElement);
-      //
-      //
-      // // AJOUTER les fonctions ci dessous
-      //
-      // addHomePage(scene, document, camera, cssrenderer);
-      //
-      //
-      //
-      // // clavier
-      // keyboardEvent(camera)
-      //
-      // // SOURIS
-      // mouseEvent(camera, controls)
-      //
-      // -----------------------------------------------------------------------------------------
-
-      // const scene = new THREE.Scene();
-      // let aspect = window.innerWidth/window.innerHeight;
-      // let cam = new THREE.PerspectiveCamera(45, aspect,0.1, 1000);
-      // cam.position.set(0,0,800);
-      //
-      //
-      // let renderer = new CSS3DRenderer();
-      // renderer.setSize(window.innerWidth, window.innerHeight);
-      // document.body.appendChild(renderer.domElement);
-      //
-      // let el = document.createElement('div');
-      // el.innerHTML = "<h1>Hello CSS3D</h1>";
-      // let obj = new CSS3DObject(el);
-      // obj.position.set(0,0,0);
-      // scene.add(obj);
-      //
-      // function draw(){
-      //   obj.rotation.y +=0.01;
-      //   renderer.render(scene, cam);
-      //   requestAnimationFrame(draw);
-      // }
-      //
-      // draw();
 
       // ----------------------------------------------------------------------------------
 
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.set(0, 0, 20);
+      camera.position.set(80, 130, 0);
 
 
       // 2. WebGLRenderer (for GLTF Models)
@@ -146,6 +38,7 @@ export class AppComponent implements AfterViewInit {
       document.body.appendChild(webGLRenderer.domElement);
       webGLRenderer.setClearColor( 0x000000, 0 ); // the default
       const controls = new OrbitControls( camera, webGLRenderer.domElement);
+      controls.target.set(0,100,0);
 
 
       // 3. CSS3DRenderer (for HTML Elements)
@@ -163,9 +56,9 @@ export class AppComponent implements AfterViewInit {
         texture.mapping = THREE.EquirectangularReflectionMapping;
         scene.environment = texture;
         const loader = new GLTFLoader();
-        loader.load('./assets/screen/scene.gltf', function (gltf) {
+        loader.load('./assets/deskLow/scene.gltf', function (gltf) {
           const model = gltf.scene;
-          model.scale.set(20, 21, 21);
+          model.scale.set(0.5, 0.5, 0.5);
           scene.add(model);
         });
       });
@@ -182,9 +75,8 @@ export class AppComponent implements AfterViewInit {
       }
 
       const homePage = new HomePageComponent();
-      homePage.create(scene, document, camera, cssRenderer).then(
-        () => animate()
-      );
+      homePage.create(scene, document, camera, cssRenderer)
+      animate();
 
 
 
--- angularPortefolio/src/app/components/home-page/home-page.component.ts
+++ angularPortefolio/src/app/components/home-page/home-page.component.ts
@@ -13,7 +13,10 @@ export class HomePageComponent {
 
 
 
-  async create(scene : THREE.Scene, document: Document, camera : THREE.PerspectiveCamera, cssRenderer : CSS3DRenderer){
+  create(scene : THREE.Scene, document: Document, camera : THREE.PerspectiveCamera, cssRenderer : CSS3DRenderer){
+
+    const html :string = "<div [className]='content-center' id='hello-css3d' style='color: white;'>Hello css3D</div>"
+    const html2 :string = "<div [className]='content-center' id='hello-css3d' style='color: white;'>Hello css3D v222</div>"
 
     // 1. renderer
     cssRenderer.setSize(window.innerWidth, window.innerHeight);
@@ -25,24 +28,26 @@ export class HomePageComponent {
 
     // 2. Page html
     const element = document.createElement('div');
-    element.style.width = '22px';
-    element.style.height = '13px';
+    element.style.width = '76px';
+    element.style.height = '40px';
     element.style.backgroundColor = 'rgba(0, 0, 0, 1)';
     element.style.color = 'rgba(0, 0, 0, 1)';
-    element.style.fontSize = '1px'; // Changer la taille du texte ici
+    element.style.fontSize = '10px'; // Changer la taille du texte ici
     element.style.position = 'absolute'; // Changer la taille du texte ici
-    element.innerHTML = "<div id='hello-css3d' style='color: white;'>Hello css3D</div>";
-
+    element.innerHTML = html;
+    element.style.paddingTop = '2px'
     document.body.appendChild(element);
 
     // On click
     const innerDiv = document.getElementById('hello-css3d');
     innerDiv?.addEventListener('click', function() {
-      element.style.backgroundColor = 'rgba(255, 0, 0, 1)'; // Changes to red
+      element.innerHTML = html2;
     });
 
+
     const cssObject = new CSS3DObject(element);
-    cssObject.position.set(0, 0, 0.8);  // Position the HTML element in the 3D space
+    cssObject.position.set(-28, 138, 8);  // Position the HTML element in the 3D space
+    cssObject.rotation.y = THREE.MathUtils.degToRad(90);
     scene.add(cssObject);
   }
 
